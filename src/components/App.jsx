9e009bf616b9bcb0659ab7fc4e54eee9f7189ae5
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-04-19 15:34:39 +0300

    like hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactsList from './contacts.json';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: ContactsList,
+    filter: '',
+  };
+
+  onSubmiHandler = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => {
+      const includeName = contacts.find(user => user.name === contact.name);
+      if (includeName) {
+        alert(`${contact.name} is already in contacts`);
+      } else {
+        return { contacts: [contact, ...contacts] };
+      }
+    });
+  };
+
+  handelChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => {
+      const newContactList = prevState.contacts.filter(
+        contact => contact.id !== id
+      );
+      console.log(newContactList);
+
+      return { contacts: newContactList };
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filterContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.onSubmiHandler} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.handelChange} />
+        <Contacts contacts={filterContacts} onDelete={this.handleDelete} />
+      </div>
+    );
+  }
+}
